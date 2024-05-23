3c480c174fc350e60cba2c1f01fdc5499d73f0f2
Joko Supriyanto <jokomanza@gmail.com>
2024-05-23 10:36:37 +0700

    change gaji data type to int

--- src/assignment/Karyawan.java
+++ src/assignment/Karyawan.java
@@ -4,13 +4,13 @@ public class Karyawan {
 
     private int nip;
     private String nama;
-    private float gaji;
+    private int gaji;
     private float lamaKerja;
 
     public Karyawan() {
     }
 
-    public Karyawan(int nip, String nama, float gaji, float lamaKerja) {
+    public Karyawan(int nip, String nama, int gaji, float lamaKerja) {
         this.nip = nip;
         this.nama = nama;
         this.gaji = gaji;
@@ -33,11 +33,11 @@ public class Karyawan {
         this.nama = nama;
     }
 
-    public float getGaji() {
+    public int getGaji() {
         return gaji;
     }
 
-    public void setGaji(float gaji) {
+    public void setGaji(int gaji) {
         this.gaji = gaji;
     }
 
